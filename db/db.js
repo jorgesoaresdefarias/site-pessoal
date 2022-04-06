23265f4dc647383618856ed8adfe82033e209d17
Jorge <jsfj@cin.ufpe.br>
2022-04-06 10:53:50 -0300

    make it better

--- db/db.js
+++ db/db.js
@@ -1,14 +1,6 @@
 const firebase = require('firebase-admin');
-const { firebaseConfig } = require('../config');
-const configs = {
-    apiKey: "AIzaSyBdWlJwWKI0bVFnYJSgdulyW6baxPL5PBU",
-    authDomain: "sitepessoal-b6700.firebaseapp.com",
-    projectId: "sitepessoal-b6700",
-    storageBucket: "sitepessoal-b6700.appspot.com",
-    messagingSenderId: "106969405687",
-    appId: "1:106969405687:web:93a3e880c8fabaac459bf2"
-}
+const config = require('../config');
 
-const db = firebase.initializeApp(configs);
+const db = firebase.initializeApp(config.firebaseConfig);
 
 module.exports = db;
