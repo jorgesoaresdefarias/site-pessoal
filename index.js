7d4aa3d249801e41b63750a4175ce886225c9430
Jorge <jsfj@cin.ufpe.br>
2022-04-06 10:37:21 -0300

    make it better

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.use(express.json());
 
 app.use('/api', routes.routes);
 
-app.listen(process.env.PORT || +config.port, () => {
+app.listen(process.env.PORT || 550, () => {
     console.log('Servidor Inicializado com Sucesso!');
 });
